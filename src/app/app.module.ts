fd06b516de3038149541a1fade601ac66e85b15a
Malik-G <Malik-G@users.noreply.github.com>
2020-04-06 20:32:30 -0500

    added a tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ImageBoxComponent } from './image-box/image-box.component';
 import { TitleComponent } from './image-box/title/title.component';
 import { GetDataService } from './image-box/get-data.service';
+import { TabsComponentComponent } from './tabs-component/tabs-component.component';
 
 @NgModule({
-  declarations: [AppComponent, ImageBoxComponent, TitleComponent],
+  declarations: [AppComponent, ImageBoxComponent, TitleComponent, TabsComponentComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [GetDataService],
   bootstrap: [AppComponent]
